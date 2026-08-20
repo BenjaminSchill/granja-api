ec794c9e4c42abed38671850cc8552c6fc3b1a86
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-20 18:46:07 -0300

    First commit: Spring Boot initial framework

---
+++ src/main/java/com/granjas/granjaapi/GranjaapiApplication.java
@@ -0,0 +1,13 @@
+package com.granjas.granjaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/granjas/granjaapi/GranjaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granjas.granjaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
