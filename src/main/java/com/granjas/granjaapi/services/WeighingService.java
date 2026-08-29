beddc4f74a0e59281fae2e43f77b585e1b71b637
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-08-29 11:44:45 -0300

    Implement automatic weight and average calculation inside Weighing insert logic

--- src/main/java/com/granjas/granjaapi/services/WeighingService.java
+++ src/main/java/com/granjas/granjaapi/services/WeighingService.java
@@ -5,16 +5,20 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.granjas.granjaapi.entities.DailyLog;
 import com.granjas.granjaapi.entities.Weighing;
+import com.granjas.granjaapi.repositories.DailyLogRepository;
 import com.granjas.granjaapi.repositories.WeighingRepository;
 
 @Service
 public class WeighingService {
 	
+	private final DailyLogRepository dailyLogRepository;
 	private final WeighingRepository weighingRepository;
 
-	public WeighingService(WeighingRepository weighingRepository) { 
+	public WeighingService(WeighingRepository weighingRepository, DailyLogRepository dailyLogRepository) { 
 		this.weighingRepository = weighingRepository;
+		this.dailyLogRepository = dailyLogRepository;
 	}
 	
 	public List<Weighing> findAll() { 
@@ -27,6 +31,23 @@ public class WeighingService {
 	}
 	
 	public Weighing insert(Weighing weighing) { 
+		DailyLog dailyLog = dailyLogRepository.findByIdWithWeighings(weighing.getDailyLog().getId()).get();
+		double totalWeight = 0.0;
+		int totalChickens = 0; 
+		
+		for (Weighing w : dailyLog.getWeighings()) {
+			totalWeight += w.getWeightInBox();
+			totalChickens += w.getTotalInBox();
+		}
+		totalWeight += weighing.getWeightInBox();
+		totalChickens += weighing.getTotalInBox();
+		
+		double averageWeight = totalWeight / (double) totalChickens;
+		dailyLog.setAverageWeight(averageWeight);
+		dailyLog.setTotalWeight(totalWeight);
+		
+		dailyLogRepository.save(dailyLog);
+		
 		return weighingRepository.save(weighing);
 	}
 }
