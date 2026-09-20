6e07bae4415f60600721fcbf0c396661d2533544
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-09-20 20:42:05 -0300

    Inject UserDetailsService and configure BCryptPasswordEncoder bean in SecurityConfig

--- src/main/java/com/granjas/granjaapi/config/SecurityConfig.java
+++ src/main/java/com/granjas/granjaapi/config/SecurityConfig.java
@@ -3,10 +3,25 @@ package com.granjas.granjaapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.granjas.granjaapi.services.UserDetailsServiceImpl;
 
 @Configuration
 public class SecurityConfig {
+	
+	private final UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	public SecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl) { 
+		this.userDetailsServiceImpl = userDetailsServiceImpl;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() { 
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
