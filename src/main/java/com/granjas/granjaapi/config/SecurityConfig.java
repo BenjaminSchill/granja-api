2c3908f561a57e085a6d25af5976e6e5a210eca5
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-09-20 22:19:10 -0300

    Configure AuthenticationManager with UserDetailsService and PasswordEncoder

--- src/main/java/com/granjas/granjaapi/config/SecurityConfig.java
+++ src/main/java/com/granjas/granjaapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.granjas.granjaapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +26,9 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+		http.getSharedObject(AuthenticationManagerBuilder.class)
+		.userDetailsService(userDetailsServiceImpl)
+		.passwordEncoder(passwordEncoder());
 		http.csrf(csrf -> csrf.disable());
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
