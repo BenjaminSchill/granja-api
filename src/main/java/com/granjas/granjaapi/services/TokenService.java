33091f640c6712d158e9f90a065e9c633a5c1284
Benjamin Schill <benjamin.schill.dev@gmail.com>
2026-09-21 20:23:20 -0300

    Implement generateToken method in TokenService using Auth0 library

--- src/main/java/com/granjas/granjaapi/services/TokenService.java
+++ src/main/java/com/granjas/granjaapi/services/TokenService.java
@@ -1,7 +1,13 @@
 package com.granjas.granjaapi.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.granjas.granjaapi.config.UserSS;
 
 @Service
 public class TokenService {
@@ -12,4 +18,13 @@ public class TokenService {
 	@Value("${api.token.expiration}")
 	private Long expiration;
 	
+	public String generateToken(UserSS userSS) { 
+		String token = JWT.create()
+				.withIssuer("granja-api")
+				.withSubject(userSS.getUsername())
+				.withExpiresAt(Instant.now().plusMillis(expiration))
+				.sign(Algorithm.HMAC256(secret));
+		
+		return token;
+	}
 }
